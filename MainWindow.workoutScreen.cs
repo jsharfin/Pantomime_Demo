e80c019628a8b4f2a6901e8f2813b8aac5b34077
Ashwin Ramanathan <ashwinnsd@gmail.com>
2016-05-06 12:22:32 -0400

    added feedback mechanism

--- MainWindow.workoutScreen.cs
+++ MainWindow.workoutScreen.cs
@@ -3,6 +3,8 @@ using System.Windows;
 using System.Windows.Media;
 using System.Drawing.Drawing2D;
 using Microsoft.Kinect;
+using System.Windows.Shapes;
+
 namespace PantomimeDemo
 {
     public partial class MainWindow : Window
@@ -171,6 +173,7 @@ namespace PantomimeDemo
                 switch (exercise)
                 {
                     case Exercise.BicepCurl:
+                        drawFeedback( dc,skel,Exercise.BicepCurl,ReadyAngles);
                         if (ReadyAngles[0] < _AngExEnd && ReadyAngles[2] < _AngExEnd)
                         {
                             _ExState = true;
@@ -192,8 +195,92 @@ namespace PantomimeDemo
             }
         }
 
+        private void drawFeedback(DrawingContext dc,Skeleton skel,Exercise ex, byte[] ReadyAngles)
+        {
+            if (ex == Exercise.BicepCurl)
+            {
+                if (ReadyAngles[1] > 40)
+                {
+                    Point a1 = this.SkeletonPointToScreen(skel.Joints[JointType.ElbowRight].Position);// new Point(skel.Joints[JointType.ShoulderRight].Position.X, skel.Joints[JointType.ElbowRight].Position.Y);
+
+                    double X1 = a1.X - 40;
+                    double Y1 = a1.Y+10;
+
+                    double X2 = a1.X - 70;
+                    double Y2 = a1.Y + 30;
+                    DrawLinkArrow(dc, new Point(X1, Y1), new Point(X2, Y2));
+                    
+                }
+                if (ReadyAngles[3] > 40)
+                {
+                    Point a1 = this.SkeletonPointToScreen(skel.Joints[JointType.ElbowLeft].Position);// new Point(skel.Joints[JointType.ShoulderRight].Position.X, skel.Joints[JointType.ElbowRight].Position.Y);
+
+                    double X1 = a1.X + 40;
+                    double Y1 = a1.Y + 10;
+
+                    double X2 = a1.X + 70;
+                    double Y2 = a1.Y + 30;
+                    DrawLinkArrow(dc, new Point(X1, Y1), new Point(X2, Y2));
+
+
+                }
+
+            }
+        }
+
+        private void DrawLinkArrow(DrawingContext dc, Point p1, Point p2)
+        {
+            GeometryGroup lineGroup = new GeometryGroup();
+            double theta = Math.Atan2((p2.Y - p1.Y), (p2.X - p1.X)) * 180 / Math.PI;
+
+            PathGeometry pathGeometry = new PathGeometry();
+            PathFigure pathFigure = new PathFigure();
+            Point p = new Point(p1.X + ((p2.X - p1.X) / 1.35), p1.Y + ((p2.Y - p1.Y) / 1.35));
+            pathFigure.StartPoint = p;
+
+            Point lpoint = new Point(p.X+6, p.Y+15 );//+6,+15
+            Point rpoint = new Point(p.X -6, p.Y+15 );//-6,15
+            LineSegment seg1 = new LineSegment();
+            seg1.Point = lpoint;
+            pathFigure.Segments.Add(seg1);
+
+            LineSegment seg2 = new LineSegment();
+            seg2.Point = rpoint;
+            pathFigure.Segments.Add(seg2);
+
+            LineSegment seg3 = new LineSegment();
+            seg3.Point = p;
+            pathFigure.Segments.Add(seg3);
+
+            pathGeometry.Figures.Add(pathFigure);
+            RotateTransform transform = new RotateTransform();
+            transform.Angle = theta + 90;
+            transform.CenterX = p.X;
+            transform.CenterY = p.Y;
+            pathGeometry.Transform = transform;
+            lineGroup.Children.Add(pathGeometry);
+
+            LineGeometry connectorGeometry = new LineGeometry();
+            connectorGeometry.StartPoint = p1;
+            connectorGeometry.EndPoint = p2;
+            lineGroup.Children.Add(connectorGeometry);
+
+            dc.DrawGeometry(Brushes.Black, new Pen(Brushes.Red,3), lineGroup);
+
+
+            //System.Windows.Shapes.Path path = new System.Windows.Shapes.Path();
+            //path.Data = lineGroup;
+            //path.StrokeThickness = 10;
+            //path.Stroke = path.Fill = Brushes.Black;
+
+       //     return path;
+        }
+
+
 
     }
+
+
 
 
 }
